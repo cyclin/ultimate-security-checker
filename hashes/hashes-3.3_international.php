5e82817135b0ea85cbc1fc6bea88e1b0b2ce1e25
Stanislav Proshkin <stanislav.proshkin@gmail.com>
2011-12-14 21:10:43 +0200

    hashes for 3.3

---
+++ hashes/hashes-3.3_international.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @author Arashi
+ * @copyright 2011
+ */
+$es_ES = array(
+'readme.html' => 'c3a8c8bb10d702d53305a2847a6de641',
+'wp-admin/setup-config.php' => 'b64730eb18c2d393627bdcc0387450c8',
+'wp-config-sample.php' => '9e2be45499383d2f8969e3a093d7e137',
+'wp-includes/functions.php' => '8063a4eadc0eb5901b80bc5c9652098e',
+'wp-includes/load.php' => '05dd4c51f1819aa6ad235173cfaea112',
+'wp-includes/ms-load.php' => 'a4e8c4ca906118353c38b1c71a9e0cb5',
+'wp-includes/ms-settings.php' => '0863675ec522fcefd700d268c61fded9',
+'wp-includes/version.php' => 'f341e5ac621b0fc1cca41554dc5e4822',
+'wp-includes/wp-db.php' => '7be0b969a1df55b1ad1627a5a0168a2f',
+'wp-load.php' => '78492f2e60d31dfc13b771c53c5f75be',
+);
+$fr_FR = array(
+'readme.html' => 'e1b3cc4970bfb79f176b551606f2c566',
+'wp-admin/setup-config.php' => '6df6844a56acad8529211ce44c13b877',
+'wp-config-sample.php' => 'eb8258245200f17d67676c0f3beaf356',
+'wp-includes/functions.php' => '30ada4faa444fd138bf1fee380416331',
+'wp-includes/load.php' => '700c6cf052d6bc04a263c951d4bc1354',
+'wp-includes/ms-load.php' => 'd2f654bac68e92ae2e7d78b18bc0d434',
+'wp-includes/ms-settings.php' => '6dcf2962310155e3d28c3453e1390ceb',
+'wp-includes/version.php' => '6f8f0a7f8816d5c5caa5cb199ff3a930',
+'wp-includes/wp-db.php' => '798e6df15750cff04c8642d4b0b831c8',
+'wp-load.php' => 'c095350f90e5a737dbbd6215a83d49bd',
+);
+/*$it_IT = array(
+
+);*/
+$ru_RU = array(
+'readme.html' => '43d66bcbb27fd050cec88499a14db994',
+'wp-admin/load-scripts.php' => '60b170db4b62b89457dfb7451fd0c045',
+'wp-admin/setup-config.php' => '7dd9415a0269e9fe7dc1cbdbd2e869da',
+'wp-config-sample.php' => '401b2ea26736a5ac879fa0f8cd247e12',
+'wp-includes/functions.php' => '8da215c04b9f410fc15781ba17de5200',
+'wp-includes/load.php' => '4a7700fe91b63f40145dc235cadc2712',
+'wp-includes/ms-load.php' => '8bc6e2e627d214e0b40292f8fccca2f9',
+'wp-includes/ms-settings.php' => '42559cc8b3c1e7502473d86dd321c59b',
+'wp-includes/version.php' => '3a55e132b33a7d2350dbe8650c604ef3',
+'wp-includes/wp-db.php' => '734d8634f6685fffbb1f051cfca1a402',
+'wp-load.php' => '78fcd11f4268b65a8e3278c0684cd598',
+);
+/*$uk = array(
+
+);*/
+$de_DE = array(
+'wp-admin/setup-config.php' => 'ff9c098f1466f7492d00f6fcf2926cf1',
+'wp-config-sample.php' => 'ec27d47c1dee04ec0dc34ef878c408be',
+'wp-includes/functions.php' => '848456bf97cee437ccb63c99cfd310b7',
+'wp-includes/load.php' => '22f4c0cc74a7c2db8de10a9214340121',
+'wp-includes/ms-load.php' => 'd0167601cafef9dd2170b3f14be96a96',
+'wp-includes/ms-settings.php' => 'ef2949b1ecec313562409e037fa698a5',
+'wp-includes/version.php' => '768f0a720553fa6f9762ccc262ce2b61',
+'wp-includes/wp-db.php' => '0f45c3dbdb8724bf87d30fabd3b2f464',
+'wp-load.php' => 'b6d8f497fa3ec89f6f154324233787bf',
+);
+?>
