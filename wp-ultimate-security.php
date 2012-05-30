1da14da97405a343df6afd9fd67e64f9647c657c
Stanislav Proshkin <stanislav.proshkin@gmail.com>
2012-05-30 20:26:08 +0300

    Current Status tab content moved to dashboard view of PRO tab Failed logins logger added

--- wp-ultimate-security.php
+++ wp-ultimate-security.php
@@ -123,10 +123,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
             </style>
             <h3 class="nav-tab-wrapper">
                     <a href="?page=ultimate-security-checker&tab=run-the-tests" class="nav-tab"><?php _e('Run the Tests');?></a>
-					<a href="?page=ultimate-security-checker&tab=current-status" class="nav-tab">Current Status</a>
                     <a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab"><?php _e('Files Analysis');?></a>
                     <a href="?page=ultimate-security-checker&tab=how-to-fix" class="nav-tab nav-tab-active"><?php _e('How to Fix');?></a>
                     <a href="?page=ultimate-security-checker&tab=settings" class="nav-tab"><?php _e('Settings');?></a>
+                    <a href="?page=ultimate-security-checker&tab=pro" class="nav-tab"><?php _e('PRO Checks');?></a>
             </h3>
 <!--			<p style="border:2px solid #eee;margin-left:3px;background:#f5f5f5;padding:10px;width:706px;font-size:14px;color:green;font-family:helvetica;">
 				Please check out our new idea: <strong>WP AppStore</strong>. 1-click install best plugins and themes.
@@ -470,10 +470,10 @@ if (strpos($_SERVER[\'REQUEST_URI\'], "eval(") ||
     
                 <h3 class="nav-tab-wrapper">
                     <a href="?page=ultimate-security-checker&tab=run-the-tests" class="nav-tab"><?php _e('Run the Tests');?></a>                    
-					<a href="?page=ultimate-security-checker&tab=current-status" class="nav-tab">Current Status</a>
-                    <a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab"><?php _e('Files Analysis');?></a>
+					<a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab"><?php _e('Files Analysis');?></a>
                     <a href="?page=ultimate-security-checker&tab=how-to-fix" class="nav-tab"><?php _e('How to Fix');?></a>
                     <a href="?page=ultimate-security-checker&tab=settings" class="nav-tab nav-tab-active"><?php _e('Settings');?></a>
+                    <a href="?page=ultimate-security-checker&tab=pro" class="nav-tab"><?php _e('PRO Checks');?></a>
                 </h3>
 <!--    			<p style="border:2px solid #eee;margin-left:3px;background:#f5f5f5;padding:10px;width:706px;font-size:14px;color:green;font-family:helvetica;">
 					Please check out our new idea: <strong>WP AppStore</strong>. 1-click install best plugins and themes.
@@ -564,6 +564,15 @@ if (strpos($_SERVER[\'REQUEST_URI\'], "eval(") ||
 								$view = 'ftp';
 								break;
 					}
+   		       $apikey = get_option('wp_ultimate_security_checker_apikey');
+		       $linkedto = get_option('wp_ultimate_security_checker_linkedto', '');
+               $params['apikey'] = $apikey;
+		       $params['blog_url'] = get_option('siteurl');		
+        	   if ($linkedto) {
+        			$params['blog_id'] = $linkedto;
+        	   }
+		       $status_url = sprintf("http://beta.ultimateblogsecurity.com/api/%s/?%s", "get_status", http_build_query($params));
+		       $find_url = sprintf("http://beta.ultimateblogsecurity.com/api/%s/?%s", "find_ftppath", http_build_query($params));
             ?>
             
             <div class="wrap">
@@ -592,8 +601,7 @@ if (strpos($_SERVER[\'REQUEST_URI\'], "eval(") ||
     
                 <h3 class="nav-tab-wrapper">
                     <a href="?page=ultimate-security-checker&tab=run-the-tests" class="nav-tab"><?php _e('Run the Tests');?></a>                    
-					<a href="?page=ultimate-security-checker&tab=current-status" class="nav-tab">Current Status</a>
-                    <a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab"><?php _e('Files Analysis');?></a>
+					<a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab"><?php _e('Files Analysis');?></a>
                     <a href="?page=ultimate-security-checker&tab=how-to-fix" class="nav-tab"><?php _e('How to Fix');?></a>
                     <a href="?page=ultimate-security-checker&tab=settings" class="nav-tab"><?php _e('Settings');?></a>
                     <a href="?page=ultimate-security-checker&tab=pro" class="nav-tab nav-tab-active"><?php _e('PRO Checks');?></a>
@@ -769,8 +777,185 @@ if (strpos($_SERVER[\'REQUEST_URI\'], "eval(") ||
                     </form>
                 <?php endif; ?>
                 <?php if($view == 'dashboard'): ?>
+                    <script type="text/javascript">
+                		jQuery(document).ready(function($) {			
+                			$('#select_website').submit(submit_selected_site);
+                			
+                			// auto start of info request
+                			$('#ajax_loading').fadeIn();
+                			// TODO: if linked and response is not found - reset state.
+                			$.ajax({
+                				url: "<?php echo $status_url;?>&callback=?",
+                				dataType: "jsonp",
+                				complete: function (){
+                					$('#ajax_loading').fadeOut();
+                				},
+                				success: function(response) {
+                					if (response && response.state == 'ok') {
+                						$('#ajax_status').show();
+                						var path_status = $('#path_status');
+                						var login_status = $('#login_status');
+                						if (response.data.path && response.data.verified) {
+                							path_status.text(response.data.path +" was successfully verified").css('color', 'green');
+                						} else if (!response.data.path) {	
+                							path_status.html('<span> was not providen yet - <a id="verify_path" href="#"> click to find</a></span>').css('color', 'red');
+                						} else {
+                							var span = document.createElement('span');
+                							$(span).text(response.data.path +" is not verified yet").css('color', 'orangered');
+                							path_status.append(span);
+                							path_status.append('<span> - <a id="verify_path" href="#"> verify</a></span>');
+                						}
+                						if (response.data.last_login) {	
+                							var status = response.data.last_login_status;
+                							var msg = status ? 'successful' : 'failed';
+                							var color = status ? 'green' : 'orangered';
+                							var d = new Date(response.data.last_login*1000);								
+                							login_status.text(msg + ' at ' + d.toLocaleString()).css('color', color);
+                						}						
+                					} else {
+                						var message;
+                						if (response.state == 'error')  {
+                							switch (response.errno) {
+                								case -2: // Blog not found
+                									add_website();
+                									return;
+                								case -3: // Multiple blogs found
+                									select_website(response.data);
+                									return;
+                								case -1: // Invalid API key									
+                								case -4: // Bad request
+                									message = response.message;
+                									break;
+                								default:
+                									message = 'unknown error occured';
+                									break;
+                							}
+                						} else {
+                							message = "can't connect to UBS server";
+                						}
+                						var err_message = '<p>Error: '+ message + '</p>';															
+                						var ajax_error = $('#ajax_error');
+                						if (!ajax_error.length) {
+                							$('#ajax_status').before('<div id="ajax_error" style="color:orangered">'+ err_message +'</div>');
+                							var ajax_error = $('#ajax_error');
+                						} else {
+                							ajax_error.text(err_message);
+                						}
+                						if (response.data) {
+                							for (item in response.data) {
+                								ajax_error.append('<p>' + item + ': ' + response.data[item] + '</p>');
+                							}		
+                						}
+                						$('#ajax_status').hide();
+                					}
+                				}
+                			});
+                			$("#verify_path").live("click", function(e){
+                				e.preventDefault();
+                				$('#ajax_loading').fadeIn();
+                				$.ajax({
+                					url: "<?php echo $find_url;?>&callback=?&path=<?php echo ABSPATH;?>",
+                					dataType: "jsonp",
+                					complete: function (){
+                						$('#ajax_loading').fadeOut();
+                					},						
+                					success: function(response) {
+                						if (response && response.state == 'ok') {
+                							$('#ajax_status').show();
+                							var path_status = $('#path_status');
+                							var login_status = $('#login_status');
+                							if (response.data.path && response.data.verified) {
+                								path_status.text(response.data.path +" was successfully verified").css('color', 'green');
+                							} else if (!response.data.path) {	
+                								path_status.text(' was not providen yet').css('color', 'red');
+                							} else {
+                								var span = document.createElement('span');
+                								$(span).text(response.data.path +" is not verified yet").css('color', 'orangered');
+                								path_status.append(span);
+                								path_status.append('<span> - <a id="verify_path" href="#"> verify</a></span>');
+                							}													
+                						} else {
+                							var msg = (response.state == 'error') ? response.message : "can't connect to UBS server";
+                							var err_message = '<p>Error: '+ msg + ' (<a id="verify_path" href="#">retry</a>) </p>';															
+                							var ajax_error = $('#ajax_error');
+                							if (!ajax_error.length) {
+                								$('#ajax_status').before('<div id="ajax_error" style="color:orangered">'+ err_message +'</div>');
+                								var ajax_error = $('#ajax_error');
+                							} else {
+                								ajax_error.text(err_message);
+                							}
+                							if (response.data) {
+                								for (item in response.data) {
+                									ajax_error.append('<p>' + item + ': ' + response.data[item] + '</p>');
+                								}		
+                							}
+                							$('#ajax_status').hide();
+                						}
+                					}	
+                				});				
+                			});
+                		});
+                        </script>
                     <h2><?php _e('Dashboard');?></h2>
-                    <p>Placeholder</p>
+                    <h4>Current status <img id="ajax_loading" style="margin-left:15px;" src="<?php echo plugins_url( 'img/loader.gif', __FILE__ ); ?>" alt="loading" /></h4>
+            		<form id="add_website" style="display: none;" action="." method="GET">
+            			<?php if ($apikey): ?>
+            				<p>Seems like you didn't added your blog at ultimateblogsecurity.com so far, you can do it right now: </p>
+            				<input type="hidden" name="apikey" value="<?php echo htmlspecialchars($apikey);?>"/>
+            				<input type="hidden" name="uri" value="<?php echo get_option('siteurl');?>"/>
+            				<table>
+            					<tr>
+            						<td><label>What's the FTP address of your blog (example: ftp://myblog.com)?</label></td>
+            						<td><input type="text" name="ftphost"/></td>
+            					</tr>
+            					<tr>
+            						<td><label>WordPress location (see settings tab in plugin)</label></td>
+            						<td><input type="text" name="ftppath" value="<?php echo ABSPATH;?>"/></td>
+            					</tr>
+            					<tr>
+            						<td><label>What's the FTP username for your blog's FTP account?</label></td>
+            						<td><input type="text" name="ftpuser"/></td>
+            					</tr>
+            					<tr>
+            						<td><label>What's the password for your blog's FTP account?</label></td>
+            						<td><input type="password" name="ftppass"/></td>
+            					</tr>
+            					<tr>
+            						<td></td>
+            						<td><input type="submit" value="Submit" style="float:right"/></td>
+            					</tr>
+            				</table>
+            			<?php else: ?>
+            				<p>If you already have account at ultimateblogsecurity.com - update APIKEY field in
+            				plugin's settings with key displayed at account info page. Otherwise, create new account first.</p>
+            			<?php endif; ?>
+            		</form>
+            		<form id="select_website" style="display:none">
+            			<p>
+            				You have multiple records in UBS dashboard for this blog.
+            				Please choose one, guided by it's FTP info.
+            			</p>
+            		</form>
+            		<table id="ajax_status">
+            			<tr>
+            				<td>Path</td><td id="path_status"></td>
+            			</tr>
+            			<tr>
+            				<td>Last login</td><td id="login_status"></td>
+            			</tr>
+            		</table>
+                    <table>
+                    <?php
+                    $failed_logins = get_option('wp_ultimate_security_checker_failed_login_attempts_log');
+                    if (is_array($failed_logins)) {
+                        foreach ($failed_logins as $key => $row) {
+                        echo "<tr>";
+                        echo ("<td>$key</td><td>{$row['time']}</td><td>{$row['username']}</td><td>{$row['ip']}</td>");
+                        echo "</tr>";
+                        }
+                    }
+                    ?>
+                    </table>			
                 <?php endif; ?>
                     <div class="clear"></div>
                     </div>
@@ -1000,10 +1185,10 @@ add_action( 'wp_ajax_ultimate_security_checker_ajax_handler', 'wp_ultimate_secur
             <?php endif; ?>
             <h3 class="nav-tab-wrapper">
                     <a href="?page=ultimate-security-checker&tab=run-the-tests" class="nav-tab"><?php _e('Run the Tests');?></a>                    
-					<a href="?page=ultimate-security-checker&tab=current-status" class="nav-tab">Current Status</a>
-                    <a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab nav-tab-active"><?php _e('Files Analysis');?></a>
+					<a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab nav-tab-active"><?php _e('Files Analysis');?></a>
                     <a href="?page=ultimate-security-checker&tab=how-to-fix" class="nav-tab"><?php _e('How to Fix');?></a>
                     <a href="?page=ultimate-security-checker&tab=settings" class="nav-tab"><?php _e('Settings');?></a>
+                    <a href="?page=ultimate-security-checker&tab=pro" class="nav-tab"><?php _e('PRO Checks');?></a>
             </h3>
 <!--			<p style="border:2px solid #eee;margin-left:3px;background:#f5f5f5;padding:10px;width:706px;font-size:14px;color:green;font-family:helvetica;">
 				Please check out our new idea: <strong>WP AppStore</strong>. 1-click install best plugins and themes.
@@ -1484,7 +1669,6 @@ add_action( 'wp_ajax_ultimate_security_checker_ajax_handler', 'wp_ultimate_secur
 		</style>
 		<h3 class="nav-tab-wrapper">
 				<a href="?page=ultimate-security-checker&tab=run-the-tests" class="nav-tab">Run the Tests</a>
-				<a href="?page=ultimate-security-checker&tab=current-status" class="nav-tab nav-tab-active">Current Status</a>
 				<a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab">Files Analysis</a>
 				<a href="?page=ultimate-security-checker&tab=how-to-fix" class="nav-tab">How to Fix</a>
 				<a href="?page=ultimate-security-checker&tab=settings" class="nav-tab">Settings</a>
@@ -1572,10 +1756,10 @@ add_action( 'wp_ajax_ultimate_security_checker_ajax_handler', 'wp_ultimate_secur
             </style>
             <h3 class="nav-tab-wrapper">
                     <a href="?page=ultimate-security-checker&tab=run-the-tests" class="nav-tab nav-tab-active"><?php _e('Run the Tests');?></a>
-                    <a href="?page=ultimate-security-checker&tab=current-status" class="nav-tab">Current Status</a>
                     <a href="?page=ultimate-security-checker&tab=wp-files" class="nav-tab"><?php _e('Files Analysis');?></a>
                     <a href="?page=ultimate-security-checker&tab=how-to-fix" class="nav-tab"><?php _e('How to Fix');?></a>
                     <a href="?page=ultimate-security-checker&tab=settings" class="nav-tab"><?php _e('Settings');?></a>
+                    <a href="?page=ultimate-security-checker&tab=pro" class="nav-tab"><?php _e('PRO Checks');?></a>
             </h3>
 <!--			<p style="border:2px solid #eee;margin-left:3px;background:#f5f5f5;padding:10px;width:706px;font-size:14px;color:green;font-family:helvetica;">
 				Please check out our new idea: <strong>WP AppStore</strong>. 1-click install best plugins and themes.
@@ -1643,6 +1827,31 @@ add_action( 'wp_ajax_ultimate_security_checker_ajax_handler', 'wp_ultimate_secur
             }
         }           
     }
+    function wp_ultimate_security_checker_failed_login_logger($username){
+        $ip = wp_ultimate_security_checker_get_address();
+        if (!$failed_attepts_log = get_option('wp_ultimate_security_checker_failed_login_attempts_log'))
+            $failed_attepts_log = array();
+        $failed_attepts_log[] = array(
+        'ip' => $ip,
+        'username' => $username,
+        'time' => date('Y-m-d H:i:s'),
+        );
+        update_option('wp_ultimate_security_checker_failed_login_attempts_log', $failed_attepts_log);  
+    }
+    function  wp_ultimate_security_checker_get_address($type = '') {
+	if (empty($type)) {
+		$type = 'REMOTE_ADDR';
+	}
+
+	if (isset($_SERVER[$type])) {
+		return $_SERVER[$type];
+	}
+    $type = 'HTTP_X_FORWARDED_FOR';
+    if (isset($_SERVER[$type])) {
+		return $_SERVER[$type];
+	}
+	return '';
+}
 // JSON functions    
 if ( !function_exists('json_decode') ){
 function json_decode($json)
@@ -1734,4 +1943,5 @@ function json_encode( $data ) {
     add_action( 'admin_bar_menu', 'wp_ultimate_security_checker_add_menu_admin_bar' ,  70);
     add_action('admin_init', 'wp_ultimate_security_checker_admin_init');
     add_action('admin_menu', 'wp_ultimate_security_checker_admin_menu');
+    add_action('wp_login_failed', 'wp_ultimate_security_checker_failed_login_logger');
 ?>
