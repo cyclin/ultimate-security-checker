a6bd9f6870a6eb803de4fd401fe2e2eac432069d
Stanislav Proshkin <stanislav.proshkin@gmail.com>
2012-07-02 13:47:27 +0300

    new hashes for 3.3.3, 3.4, 3.4.1 versions

---
+++ hashes/hashes-3.4.1_international.php
@@ -0,0 +1,23 @@
+<?php
+$es_ES = array(
+'license.txt' => '405836dc36b41ce662dba3423eab616c',
+'readme.html' => '7fd57d9b98f3e6df640f2131d7e4c896',
+'wp-admin/setup-config.php' => 'b64730eb18c2d393627bdcc0387450c8',
+'wp-config-sample.php' => '9e2be45499383d2f8969e3a093d7e137',
+'wp-includes/version.php' => '238b1b22e12c2297fcbff86ae41d924a',
+);
+$de_DE = array(
+'wp-config-sample.php' => 'ec27d47c1dee04ec0dc34ef878c408be',
+'wp-includes/version.php' => '11099f85146e789ee95cd885ea12cf2d',
+);
+$ru_RU = array(
+'readme.html' => '7d882b3bce9d09c541a7ec3da436a2e7',
+'wp-config-sample.php' => '401b2ea26736a5ac879fa0f8cd247e12',
+'wp-includes/version.php' => '05f82723de3b3ab07bfe3e87f7686fbe',
+);
+$fr_FR = array(
+'readme.html' => 'b338edd735370996be58ca038f6e2f4a',
+'wp-config-sample.php' => 'eb8258245200f17d67676c0f3beaf356',
+'wp-includes/version.php' => 'ea95492e745d95a5bbac57c0f8ab0e87',
+);
+?>
---
+++ hashes/hashes-3.4_international.php
@@ -0,0 +1,28 @@
+<?php
+$es_ES = array(
+'license.txt' => '405836dc36b41ce662dba3423eab616c',
+'readme.html' => 'ceb48c3b5552462b4f7fa91e46fb7ac4',
+'wp-admin/setup-config.php' => 'b64730eb18c2d393627bdcc0387450c8',
+'wp-config-sample.php' => '9e2be45499383d2f8969e3a093d7e137',
+'wp-includes/version.php' => '474486b38e123412ad26d1bee2e1442b',
+);
+$it_IT = array(
+'readme.html' => 'f83f498a387c13e0515ac80123251937',
+'wp-config-sample.php' => '9d58ffdbd22e9d26f89e2a54ecb8f3c7',
+'wp-includes/version.php' => 'ed5ef66234b8f7242a34a7907c733fc1',
+);
+$de_DE = array(
+'wp-config-sample.php' => 'ec27d47c1dee04ec0dc34ef878c408be',
+'wp-includes/version.php' => '3f72adf7063c11383112f39f026bd3ec',
+);
+$ru_RU = array(
+'readme.html' => '0e7e6a6b2a746853eafd6b1bc793d898',
+'wp-config-sample.php' => '401b2ea26736a5ac879fa0f8cd247e12',
+'wp-includes/version.php' => '60e9880fd0a1f3d7c20f488c6a751fc6',
+);
+$fr_FR = array(
+'readme.html' => 'b338edd735370996be58ca038f6e2f4a',
+'wp-config-sample.php' => 'eb8258245200f17d67676c0f3beaf356',
+'wp-includes/version.php' => 'c4ec3eb99a9eb4e179363683f41a34f9',
+);
+?>
